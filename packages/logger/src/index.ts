43dfcc8402dc8f0fcd56922d692a6075cf2bdbae
brat <bharatrajsingal.tech@gmail.com>
2026-09-03 17:56:33 +0000

    init

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,4 +1,23 @@
 export const log = (...args: unknown[]): void => {
   // eslint-disable-next-line no-console -- logger
-  console.log("LOGGER: ", ...args);
+  console.log('[LOGGER]', ...args);
+};
+
+export const logger = {
+  info: (msg: string, ...args: unknown[]): void => {
+    // eslint-disable-next-line no-console -- logger
+    console.log(`[INFO] ${msg}`, ...args);
+  },
+  warn: (msg: string, ...args: unknown[]): void => {
+    // eslint-disable-next-line no-console -- logger
+    console.warn(`[WARN] ${msg}`, ...args);
+  },
+  error: (msg: string, ...args: unknown[]): void => {
+    // eslint-disable-next-line no-console -- logger
+    console.error(`[ERROR] ${msg}`, ...args);
+  },
+  debug: (msg: string, ...args: unknown[]): void => {
+    // eslint-disable-next-line no-console -- logger
+    console.debug(`[DEBUG] ${msg}`, ...args);
+  },
 };
